7da8a19e1ac19617ffaf18e7b8ffbb167af2b4f8
Lloyd <lloydwestbury@gmail.com>
2019-05-07 19:50:16 +0200

    Added event creation form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,14 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
-    
     @all_events_array = Event.all
+  end
+  def show
+  end
+  def new
+    @event = Event.new
+  end
+  def create
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'static_pages/secret'
   devise_for :users
   root 'events#index'
-  resources :events, only: [:index, :show, :edit, :destroy]
+  resources :events, only: [:index, :show, :edit, :new, :create]
   resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
