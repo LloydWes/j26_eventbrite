90bdd9cae6d73860e4208faf3c5e6e00d9b4f931
Lloyd <lloydwestbury@gmail.com>
2019-05-07 22:47:54 +0200

    Finale commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :check_authenticate_user, only: [:show]
-
+  before_action :authenticate_user!, only: [:show]
   def show
     puts "#"*10
     puts params[:id]
@@ -10,8 +10,7 @@ class UsersController < ApplicationController
   private
 
   def check_authenticate_user
-    if authenticate_user!
-    elsif !user_signed_in? || current_user.id != params[:id].to_i
+    if !user_signed_in? || current_user.id != params[:id].to_i
       flash[:danger] = "You tried to access the wrong profil"
       # puts "#"*50, flash[:danger]
       redirect_to :root
